cc17afb53d27f194b0f91f6a55d991c54f426774
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-02 22:20:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import { render } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const changeText = function() {
+  console.log("Clicked!");
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={changeText} />
   </div>,
   document.getElementById('root')
 );
